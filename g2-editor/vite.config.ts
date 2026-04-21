9bb7b2c5cd9bae18a59259bbf5abebd73b1b6214
Jan den Besten <info@jandenbesten.net>
2026-04-22 01:05:55 +0200

    removed devtools

--- g2-editor/vite.config.ts
+++ g2-editor/vite.config.ts
@@ -3,14 +3,12 @@ import vue from '@vitejs/plugin-vue'
 import electron from 'vite-plugin-electron'
 import renderer from 'vite-plugin-electron-renderer'
 import tailwindcss from '@tailwindcss/vite'
-import vueDevTools from 'vite-plugin-vue-devtools'
 import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [
     vue(),
     tailwindcss(),
-    vueDevTools(),
     electron([
       {
         entry: 'electron/main.ts',
