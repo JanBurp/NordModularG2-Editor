4d715ce59ed9bcdb8a2b4671c373bffb1acf9623
Jan den Besten <info@jandenbesten.net>
2026-04-22 00:02:54 +0200

    Added vue-devtools

--- g2-editor/vite.config.ts
+++ g2-editor/vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import electron from 'vite-plugin-electron'
 import renderer from 'vite-plugin-electron-renderer'
+import vueDevTools from 'vite-plugin-vue-devtools'
 import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [
     vue(),
+    vueDevTools(),
     electron([
       {
         entry: 'electron/main.ts',
