349604985a7db3318295b34c70f9c2d5d39dc44f
Jan den Besten <info@jandenbesten.net>
2026-04-30 09:31:53 +0200

    Changed ColorPicker

--- g2-editor/src/components/common/ColorPicker.vue
+++ g2-editor/src/components/common/ColorPicker.vue
@@ -1,13 +1,9 @@
 <template>
-	<div class="relative">
-		<div :style="currentColorStyle" class="border border-neutral-600 rounded min-w-10 h-8 cursor-pointer" @click="togglePicker()"></div>
-		<div
-			v-if="pickerOpen"
-			class="z-50 absolute bg-neutral-200 grid grid-cols-4 grid-rows-4 border border-neutral-600 rounded w-40"
-			@mouseleave="togglePicker()"
-		>
+	<div class="relative" @mouseleave="closePicker()">
+		<div :style="currentColorStyle" class="border border-neutral-600 rounded min-w-10 h-8 cursor-pointer" @dblclick.stop="togglePicker()"></div>
+		<div v-if="pickerOpen" class="z-50 absolute bg-neutral-200 grid grid-cols-4 grid-rows-4 border border-neutral-600 rounded w-40">
 			<template v-for="index in MODULE_COLORS_ORDER">
-				<div v-if="index > 0" class="w-10 h-8 border border-neutral-600" :style="colorStyle(index)" @click="setColor(index)"></div>
+				<div v-if="index > 0" class="w-10 h-8 border border-neutral-600" :style="colorStyle(index)" @click.stop="setColor(index)"></div>
 			</template>
 		</div>
 	</div>
@@ -36,5 +32,9 @@
 
 	function togglePicker() {
 		pickerOpen.value = !pickerOpen.value;
+	}
+
+	function closePicker() {
+		pickerOpen.value = false;
 	}
 </script>
