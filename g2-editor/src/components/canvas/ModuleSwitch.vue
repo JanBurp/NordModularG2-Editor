afba27b538a890f55bfc7ae9c10cf535ff09e9e3
Jan den Besten <info@jandenbesten.net>
2026-05-11 09:41:53 +0200

    Fixed ActiveMonitor button

--- g2-editor/src/components/canvas/ModuleSwitch.vue
+++ g2-editor/src/components/canvas/ModuleSwitch.vue
@@ -3,13 +3,13 @@
 		<!-- Bitmap-based switch -->
 		<template v-if="hasBitmap">
 			<!-- Single button mode: show active bitmap with highlight -->
-			<svg v-if="singleButtonMode && optionNames.length === 1" :x="0" :y="0" :width="width" height="11" class="switch-bitmap" @click="onCycleValue">
+			<svg v-if="singleButtonMode && displayNames.length <= 1" :x="0" :y="0" :width="width" height="11" class="switch-bitmap" @click="onCycleValue">
 				<rect x="0" y="0" :width="width" height="11" :fill="activeIndex === 1 ? '#6df2f2' : '#CCC'" stroke="#333" />
 				<use :href="`#Bitmap${bmp}`" :clip-path="`url(#clip-${param.n}-0)`" />
 			</svg>
 
 			<!-- Single button mode: show active bitmap without highlight -->
-			<svg v-if="singleButtonMode && optionNames.length > 1" :x="0" :y="0" :width="width" height="11" class="switch-bitmap" @click="onCycleValue">
+			<svg v-else-if="singleButtonMode && displayNames.length > 1" :x="0" :y="0" :width="width" height="11" class="switch-bitmap" @click="onCycleValue">
 				<rect x="0" y="0" :width="width" height="11" fill="#EEE" stroke="#333" />
 				<use :href="`#Bitmap${bmp}`" :transform="`translate(0,${-(activeIndex * maskh)})`" :clip-path="`url(#clip-${param.n}-0)`" />
 			</svg>
@@ -35,7 +35,7 @@
 		<!-- Text-based switch -->
 		<template v-else>
 			<!-- Single button mode, with one displayname: show with highlight -->
-			<g v-if="singleButtonMode && displayNames.length < 3" class="switch-button" @click="onCycleValue">
+			<g v-if="singleButtonMode && displayNames.length <= 1" class="switch-button" @click="onCycleValue">
 				<rect :x="0" :y="0" :width="width" height="11" stroke="#333" :fill="activeIndex === 1 ? '#6df2f2' : '#CCC'" />
 				<text :x="width / 2" :y="9" fill="#000" font-size="8" text-anchor="middle" pointer-events="none">
 					{{ activeOptionName }}
@@ -43,7 +43,7 @@
 			</g>
 
 			<!-- Single button mode: show only active option without highlight -->
-			<g v-else-if="singleButtonMode && displayNames.length > 2" class="switch-button" @click="onCycleValue">
+			<g v-else-if="singleButtonMode && displayNames.length > 1" class="switch-button" @click="onCycleValue">
 				<rect :x="0" :y="0" :width="width" height="11" stroke="#333" fill="#EEE" />
 				<text :x="width / 2" :y="9" fill="#000" font-size="8" text-anchor="middle" pointer-events="none">
 					{{ activeOptionName }}
