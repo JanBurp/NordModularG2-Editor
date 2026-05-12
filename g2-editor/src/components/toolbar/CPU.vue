8e2076d93cdcc748f51c1e5a8089902f7a3a0855
Jan den Besten <info@jandenbesten.net>
2026-05-12 20:25:46 +0200

    small UX changes

--- g2-editor/src/components/toolbar/CPU.vue
+++ g2-editor/src/components/toolbar/CPU.vue
@@ -1,9 +1,9 @@
 <template>
-	<div class="h-8 flex gap-3 bg-neutral-700 rounded px-2 items-center">
-		<span class="w-3 text-xs text-neutral-400">VA</span>
+	<div class="h-8 flex gap-1 bg-neutral-700 rounded px-2 items-center">
+		<span class="w-4 text-xs text-neutral-400">VA</span>
 		<CPUpart :value="va.cycles" />
 		<CPUpart :value="va.memory" />
-		<span class="w-3 text-xs text-neutral-400">FX</span>
+		<span class="w-4 text-xs text-neutral-400">FX</span>
 		<CPUpart :value="fx.cycles" />
 		<CPUpart :value="fx.memory" />
 	</div>
--- g2-editor/src/components/toolbar/CPUpart.vue
+++ g2-editor/src/components/toolbar/CPUpart.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="w-7 h-7 flex flex-col justify-between gap-1 bg-neutral-800 border border-neutral-600 overflow-hidden">
+	<div class="w-7 h-7 flex flex-col justify-between bg-neutral-800 border border-neutral-600 overflow-hidden">
 		<span class="text-[9px] text-neutral-300 font-mono text-center">{{ value.toFixed(1) }}</span>
 		<div class="w-full h-2 bg-neutral-700 overflow-hidden">
 			<div class="h-full" :class="value >= 100 ? 'bg-red-500' : 'bg-green-500'" :style="{ width: Math.min(value, 100) + '%' }" />
