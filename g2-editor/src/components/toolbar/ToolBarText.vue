0eec1b8b134d14549cff5e409b5e87fd27a79a13
Jan den Besten <info@jandenbesten.net>
2026-05-21 16:53:15 +0200

    VoiceMode/Count working (only small UX thing left)

--- g2-editor/src/components/toolbar/ToolBarText.vue
+++ g2-editor/src/components/toolbar/ToolBarText.vue
@@ -1,5 +1,5 @@
 <template>
-	<span class="label text text-nowrap overflow-hidden text-ellipsis" :class="bgColor">
+	<span class="label text text-nowrap overflow-hidden text-ellipsis h-8" :class="bgColor">
 		<slot></slot>
 	</span>
 </template>
