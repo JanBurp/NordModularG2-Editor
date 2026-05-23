21404e6702a760d2f316f290e510d98aadfc467e
Jan den Besten <info@jandenbesten.net>
2026-05-23 15:23:51 +0200

    Styling of Browser updated

---
+++ g2-editor/src/components/browser/StateMessage.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="p-4 text-center" :class="colorClass">
+		{{ message }}
+	</div>
+</template>
+
+<script setup lang="ts">
+	import { computed } from 'vue';
+
+	const props = defineProps<{
+		variant: 'loading' | 'error' | 'empty';
+		message: string;
+	}>();
+
+	const colorClass = computed(() => {
+		switch (props.variant) {
+			case 'error':
+				return 'text-red-500';
+			case 'loading':
+			case 'empty':
+			default:
+				return 'text-neutral-500';
+		}
+	});
+</script>
