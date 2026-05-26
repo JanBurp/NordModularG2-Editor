4d2bd86103c970a4f381a42d95fd9580c19ae089
Jan den Besten <info@jandenbesten.net>
2026-05-26 17:29:42 +0200

    License added

--- g2-editor/src/main.ts
+++ g2-editor/src/main.ts
@@ -1,7 +1,11 @@
+// Copyright (C) 2026 Jan den Besten
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+import './style.css';
+
+import App from './App.vue';
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-import App from './App.vue';
-import './style.css';
 
 const app = createApp(App);
 app.use(createPinia());
