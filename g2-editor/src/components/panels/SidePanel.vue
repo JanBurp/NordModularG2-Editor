709b1250c0fe610cfe7da8207fd5cb0604752d68
Jan den Besten <info@jandenbesten.net>
2026-06-05 09:49:45 +0200

    Raw Settings Pane - needs testing
    # Conflicts:
    #	g2-editor/src/App.vue

--- g2-editor/src/components/panels/SidePanel.vue
+++ g2-editor/src/components/panels/SidePanel.vue
@@ -1,7 +1,20 @@
 <template>
-	<div class="w-72 bg-neutral-800 border-l border-neutral-700 flex flex-col">
-		<div class="flex-1 overflow-hidden p-3">
-			<slot></slot>
+	<div class="w-68 bg-neutral-800 border-l border-neutral-700 flex flex-col">
+		<div class="flex-1 overflow-hidden overflow-y-auto">
+			<SettingsPane v-show="uiStore.rightPaneTab === 'settings'" />
+			<ModulesPane v-show="uiStore.rightPaneTab === 'modules'" :isActive="uiStore.rightPaneTab === 'modules'" />
+			<PatchBrowser v-show="uiStore.rightPaneTab === 'browser'" :isActive="uiStore.rightPaneTab === 'browser'" @select="patchFile.handlePatchSelect" />
 		</div>
 	</div>
 </template>
+
+<script setup lang="ts">
+	import SettingsPane from './SettingsPane.vue';
+	import ModulesPane from './ModulesPane.vue';
+	import PatchBrowser from './PatchBrowser.vue';
+	import { useUiStore } from '@/store/ui';
+	import { usePatchFile } from '@/composables/usePatchFile';
+
+	const uiStore = useUiStore();
+	const patchFile = usePatchFile();
+</script>
